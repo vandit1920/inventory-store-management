ae53d952498c6323d7b1bea552c34a560aa4b601
vandit1920 <48818526+vandit1920@users.noreply.github.com>
2024-08-25 22:14:36 -0700

    building inventory store management project using react-redux-nextjs-aws services

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,63 @@
 import type { Config } from "tailwindcss";
+import { createThemes } from "tw-colors";
+import colors, { white } from "tailwindcss/colors"; 
+// ^^ is used for dark and light mode
+
+const baseColors = [
+  "gray",
+  "red",
+  "yellow", 
+  "green", 
+  "blue",
+  "indigo",
+  "purple",
+  "pink",
+];
+
+// map different values opposite end, if its 50 the 900 in the drak mode
+const shadeMapping = {
+  "50": "900",
+  "100": "800",
+  "200": "700",
+  "300": "600",
+  "400": "500",
+  "500": "400",
+  "600": "300",
+  "700": "200",
+  "800": "100",
+  "900": "50",
+};
+// function to use setup colors
+const generateThemeObject = (colors:any, mapping: any, invert = false) => {
+  const theme: any = {};
+  baseColors.forEach((color) => {
+    theme[color] = {};
+    Object.entries(mapping).forEach(([key, value]: any) => {
+      const shadeKey = invert ? value : key;
+      theme[color][key] = colors[color][shadeKey];
+    });
+  });
+  return theme;
+}
+
+const lightTheme = generateThemeObject(colors, shadeMapping);
+const darkTheme = generateThemeObject(colors, shadeMapping, true);
+
+
+const themes = {
+  light: {
+    ...lightTheme,
+    white: "#ffffff"
+  },
+  dark: {
+    ...darkTheme,
+    white: colors.gray["950"],
+    black: colors.gray["50"]
+  }
+}
 
 const config: Config = {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -15,6 +72,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [createThemes(themes)],
 };
 export default config;
