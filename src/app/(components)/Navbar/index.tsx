558ecc425dfcbe4646cfee898a820b825b9ba1e8
vandit1920 <48818526+vandit1920@users.noreply.github.com>
2024-08-26 01:09:56 -0700

    ndex files for nav and side bar with working functionalities

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -1,8 +1,8 @@
 "use client"; //server component, interactions as click, need to use this
 
 import { useAppDispatch, useAppSelector } from "@/app/redux";
-import { setIsSidebarCollapsed } from "@/state";
-import { Bell, Menu, Settings, Sun } from "lucide-react";
+import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
+import { Bell, Menu, Moon, Settings, Sun } from "lucide-react";
 import Link from "next/link";
 import React from 'react'
 const Navbar = () => {
@@ -12,9 +12,16 @@ const dispatch =   useAppDispatch();
     (state) => state.global.isSidebarCollapsed
   );
 
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode)  
+
   const toggleSidebar = () => {
     dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
   }; 
+
+  const toggleDarkMode = () => {
+    dispatch(setIsDarkMode(!isDarkMode));
+  };
+
   return (
     <div className="flex justify-between items-center w-full mb-7">
         {/* LEFT SIDE */}
@@ -30,7 +37,7 @@ const dispatch =   useAppDispatch();
                 <input 
                     type="search" 
                     placeholder="Start type to search groups & prodcuts"
-                    className="pl-10 pr-4 py-2 w-50 md:w-80 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500" 
+                    className="pl-10 pr-4 py-2 w-50 md:w-60 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500" 
                 />
 
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
@@ -43,8 +50,13 @@ const dispatch =   useAppDispatch();
         <div className="flex justify-between items-center gap-5">
             <div className="hidden md:flex justify-between items-center gap-5">
                 <div>
-                    <button onClick={() => {}}>
-                        <Sun className="cursor-pointer text-gray-500" size={24} />
+                    <button onClick={toggleDarkMode}>
+                        {isDarkMode ? (
+                            <Sun className="cursor-pointer text-gray-500" size={24} />
+                        ) : (
+                            <Moon className="cursor-pointer text-gray-500" size={24} />
+                        )}
+                        
                     </button>
                 </div>
                 <div className="relative">
--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -2,7 +2,7 @@
 
 import { useAppDispatch, useAppSelector } from '@/app/redux';
 import { setIsSidebarCollapsed } from '@/state';
-import { Icon, Layout, LucideIcon, Menu } from 'lucide-react'
+import { Archive, CircleDollarSign, Clipboard, Icon, Layout, LucideIcon, Menu, SlidersHorizontal, User } from 'lucide-react'
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React from 'react'
@@ -82,13 +82,24 @@ const Sidebar = () => {
 
   <div className='flex-grow-mt-8'>
     {/* LINKS here */}
+    {/* Can add more custom links */}
     <SidebarLink href="/dashboard/" icon={Layout} label='Dashboard' isCollapsed={isSidebarCollapsed}/>
+
+    <SidebarLink href="/inventory/" icon={Archive} label='Inventory' isCollapsed={isSidebarCollapsed}/>
+
+    <SidebarLink href="/products/" icon={Clipboard} label='Products' isCollapsed={isSidebarCollapsed}/>
+
+    <SidebarLink href="/users/" icon={User} label='Users' isCollapsed={isSidebarCollapsed}/>
+
+    <SidebarLink href="/settings/" icon={SlidersHorizontal} label='Settings' isCollapsed={isSidebarCollapsed}/>
+
+    <SidebarLink href="/expenses/" icon={CircleDollarSign} label='Expenses' isCollapsed={isSidebarCollapsed}/>
   </div>
 
   {/* FOOTER */}
-  <div>
-    <p className='text-center text-xs text-gray-500'>&copy; 2024 DeadStcok</p>
-  </div>
+    <div className={`${isSidebarCollapsed ? "hidden" : "block"} mb-10`}>
+    <p className="text-center text-xs text-gray-500">&copy; 2024 DeadStcok</p>
+    </div>
   </div>
   );
 };
